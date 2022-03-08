679f63cb574e94c426da0c38d364ec168aad5599
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-08 17:23:29 +0100

    Solve day 18 part 2

--- src/day18/index.js
+++ src/day18/index.js
@@ -2,20 +2,19 @@ import run from "aocrunner"
 
 const parseInput = rawInput => rawInput.split('\n').map(l => l.split('').map(c => c == '#'))
 
-const slog = state => console.log(state.map(l => l.map(c => c ? '#' : '.').join('')).join('\n'))
+const step = state => {
+  const dirs = [[0,-1],[0,1],[1,-1],[1,0],[1,1],[-1,-1],[-1,0],[-1,1]]
+
+  const turnsOn = (x, y) => {
+    const count = dirs.filter(([dx, dy]) => state[x+dx]?.[y+dy]).length
+    return count == 3 || (state[x][y] && count == 2)
+  }
+
+  return state.map((l, x) => l.map((_, y) => turnsOn(x, y)))
+}
+
 const part1 = (rawInput) => {
   var state = parseInput(rawInput)
-
-  const step = state => {
-    const dirs = [[0,-1],[0,1],[1,-1],[1,0],[1,1],[-1,-1],[-1,0],[-1,1]]
-
-    const turnsOn = (x, y) => {
-      const count = dirs.filter(([dx, dy]) => state[x+dx]?.[y+dy]).length
-      return count == 3 || (state[x][y] && count == 2)
-    }
-
-    return state.map((l, x) => l.map((_, y) => turnsOn(x, y)))
-  }
 
   for(var i = 1; i <= 100; i++) {
     state = step(state)
@@ -25,9 +24,21 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  var input = parseInput(rawInput)
+  const lightCorners = state => {
+    state[0][0] = true
+    state[state.length-1][0] = true
+    state[0][state.length-1] = true
+    state[state.length-1][state.length-1] = true
+    return state
+  }
 
-  return
+  var state = lightCorners(parseInput(rawInput))
+
+  for(var i = 1; i <= 100; i++) {
+    state = lightCorners(step(state))
+  }
+
+  return state.flat().filter(x => x).length
 }
 
 run({
