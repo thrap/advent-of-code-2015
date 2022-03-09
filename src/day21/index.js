1b0be8ce10b67f9b225432d5449182457f41ac0e
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-09 12:05:29 +0100

    Solve day 21 part 2

--- src/day21/index.js
+++ src/day21/index.js
@@ -24,63 +24,71 @@ Defense +1   20     0       1
 Defense +2   40     0       2
 Defense +3   80     0       3`.split('\n\n').map(l => l.split('\n').slice(1).map(l => l.split(/ {2,}/).slice(1).map(x => +x)))
 
+const addArr = ([a1, b1, c1], [a2, b2, c2]) => [a1 + a2, b1 + b2, c1 + c2]
+
+const playerWins = (playerHp, playerDamage, playerArmor, bossHp, bossDamage, bossArmor) => {
+  for (var i = 0; playerHp > 0 && bossHp > 0; i++) {
+    if (i % 2 == 0) {
+      bossHp -= Math.max(1, playerDamage - bossArmor)
+    } else {
+      playerHp -= Math.max(1, bossDamage - playerArmor)
+    }
+  }
+  return i % 2 == 1
+}
+
+const equipRing = (weapon, armor, tryEquipment) => {
+  const stat = addArr(weapon, armor)
+  tryEquipment(stat)
+  for (var i = 0; i < rings.length; i++) {
+    tryEquipment(addArr(stat, rings[i]))
+    for (var j = i + 1; j < rings.length; j++) {
+      tryEquipment(addArr(addArr(stat, rings[i]),rings[j]))
+    }
+  }
+}
+
+const equipArmor = (weapon, tryEquipment) => {
+  equipRing(weapon, [0,0,0], tryEquipment)
+  for (var i = 0; i < armor.length; i++) {
+    equipRing(weapon, armor[i], tryEquipment)
+  }
+}
+
+const equipPermutations = (tryEquipment) => {
+  weapons.forEach(weapon => equipArmor(weapon, tryEquipment))
+}
+
 const part1 = (rawInput) => {
   const boss = parseInput(rawInput)
-
-  const fight = (playerHp, playerDamage, playerArmor, bossHp, bossDamage, bossArmor) => {
-    for (var i = 0; playerHp > 0 && bossHp > 0; i++) {
-      if (i % 2 == 0) {
-        bossHp -= Math.max(1, playerDamage - bossArmor)
-      } else {
-        playerHp -= Math.max(1, bossDamage - playerArmor)
-      }
-    }
-    return i % 2 == 1
-  }
-
-  const player = [8, 5, 5]
-
-  const addArr = ([a1, b1, c1], [a2, b2, c2]) => [a1 + a2, b1 + b2, c1 + c2]
 
   var min = Number.MAX_VALUE
   const tryEquipment = ([cost, damage, armor]) => {
 
-    if (fight(100, damage, armor, ...boss)) {
+    if (playerWins(100, damage, armor, ...boss)) {
       min = Math.min(min, cost)
     }
   }
-  const equipRing = (weapon, armor) => {
-    const stat = addArr(weapon, armor)
-    tryEquipment(stat)
-    for (var i = 0; i < rings.length; i++) {
-      tryEquipment(addArr(stat, rings[i]))
-      for (var j = i + 1; j < rings.length; j++) {
-        tryEquipment(addArr(addArr(stat, rings[i]),rings[j]))
-      }
-    }
-  }
 
-  const equipArmor = (weapon) => {
-    equipRing(weapon, [0,0,0])
-    for (var i = 0; i < armor.length; i++) {
-      equipRing(weapon, armor[i])
-    }
-  }
-
-  const equip = () => {
-    for (var i = 0; i < weapons.length; i++) {
-      equipArmor(weapons[i])
-    }
-  }
-  equip()
+  equipPermutations(tryEquipment)
 
   return min
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const boss = parseInput(rawInput)
 
-  return
+  var max = 0
+  const tryEquipment = ([cost, damage, armor]) => {
+
+    if (!playerWins(100, damage, armor, ...boss)) {
+      max = Math.max(max, cost)
+    }
+  }
+
+  equipPermutations(tryEquipment)
+
+  return max
 }
 
 run({
