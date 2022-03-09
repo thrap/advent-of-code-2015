4306b6bfbd7e2369f2a7f6945421398fafd408b4
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-09 10:12:12 +0100

    Solve day 20 part 1

---
+++ src/day20/index.js
@@ -0,0 +1,42 @@
+import run from "aocrunner"
+
+const parseInput = rawInput => +rawInput
+
+var divisors = n => {
+  var divisors = [1,n]
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      var d2 = n / i;
+      divisors.push(i)
+      if (i != d2) {
+        divisors.push(d2)
+      }
+    }
+  }
+  return divisors
+}
+
+const part1 = (rawInput) => {
+  const goal = parseInput(rawInput)
+
+  const d = n => 10 * divisors(n).reduce((acc, x) => acc + x);
+
+  var n = 0
+  while (d(n) < goal)
+    n++
+
+  return n
+}
+
+const part2 = (rawInput) => {
+
+}
+
+run({
+  part1: {
+    solution: part1,
+  },
+  part2: {
+    solution: part2,
+  },
+})
