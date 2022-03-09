f83c58b735f24334f95fe63cdf1ae4a0d073f9ab
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2022-03-09 16:06:53 +0100

    Solve day 24 part 2

--- src/day24/index.js
+++ src/day24/index.js
@@ -4,11 +4,8 @@ const parseInput = rawInput => rawInput.split('\n').map(x => +x)
 
 const part1 = (rawInput) => {
   const packages = parseInput(rawInput).sort((a,b) => b - a)
-  console.log(packages.length);
   const sum = packages.reduce((acc, x) => acc + x)
-  console.log(packages)
   const goal = sum / 3
-  console.log(goal);
 
   var min = Number.MAX_VALUE
   var count = 0
@@ -39,9 +36,36 @@ const part1 = (rawInput) => {
 }
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+  const packages = parseInput(rawInput).sort((a,b) => b - a)
+  const sum = packages.reduce((acc, x) => acc + x)
+  const goal = sum / 4
 
-  return
+  var min = Number.MAX_VALUE
+  var count = 0
+  var minQE = Number.MAX_VALUE
+  const brute = (a, b, c, d, i, c1, qe) => {
+    if (c1 > min || qe >= minQE)
+      return
+    if (a == goal && b == goal && c == goal && d == goal) {
+      min = c1
+      minQE = qe
+
+      count ++
+      console.log(count, a, b, c, c1, qe);
+      return
+    }
+    if (i >= packages.length) return
+    if (a > goal || b > goal || c > goal || d > goal) return
+
+    const pack = packages[i]
+    brute(a + pack, b, c, d, i+1, c1 + 1, qe * pack)
+    brute(a, b + pack, c, d, i+1, c1, qe)
+    brute(a, b, c + pack, d, i+1, c1, qe)
+    brute(a, b, c, d + pack, i+1, c1, qe)
+  }
+
+  brute(0,0,0,0,0,0,1)
+  return minQE
 }
 
 const part1Input = `1
@@ -64,7 +88,7 @@ run({
   },
   part2: {
     tests: [
-      { input: part2Input, expected: "" }
+      { input: part2Input, expected: 44 }
     ],
     solution: part2,
   },
